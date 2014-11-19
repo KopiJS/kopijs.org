d063cea5513b6ba3e1d5493d91dd40970fc8773d
Lim Chee Aun <cheeaun@gmail.com>
2014-11-20 01:28:14 +0800

    More styles. Beams. Sugar cubes.

--- assets/kopiio.js
+++ assets/kopiio.js
@@ -5,16 +5,28 @@
     var $kopi = $kopis[i];
     var name = $kopi.textContent;
     var ingredients = Kopi.parse(name);
+
     var internalIngredients = 'water condensed_milk evaporated_milk coffee'.split(' ');
-    var internalHTML = internalIngredients.map(function(ingredient){
+    var internalIngredientsHTML = internalIngredients.map(function(ingredient){
       var value = ingredients[ingredient];
       if (!value) return;
       var height = value * 100 + '%';
       return '<div class="' + ingredient + '" style="height: ' + height + '"></div>';
     }).join('');
+
+    var sugarHTML = '';
+    if (ingredients.sugar > 0){
+      if (ingredients.sugar <= 0.5){
+        sugarHTML = '<div class="sugar"></div>';
+      } else {
+        sugarHTML = '<div class="sugar sugar-double"></div>';
+      }
+    }
+
     var html = '<div class="kopi">'
+        + sugarHTML
         + '<div class="cup">'
-          + '<div class="ingredients">' + internalHTML + '</div>'
+          + '<div class="ingredients">' + internalIngredientsHTML + '</div>'
         + '</div>'
         + '<div class="plate"></div>'
       + '</div>'
