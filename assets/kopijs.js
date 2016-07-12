d5e6a7c62cfd5bebf191e8966b2fc1063e681f89
Lim Chee Aun <cheeaun@gmail.com>
2016-07-12 20:27:48 +0800

    Recode the whole Kopi Maker thing to be more friendly for users
    Sorry @stanleynguyen , but thanks a lot for the inspiration!

--- assets/kopijs.js
+++ assets/kopijs.js
@@ -20,106 +20,100 @@
   for (var i=0, l=$kopis.length; i<l; i++){
     var $kopi = $kopis[i];
     var name = $kopi.textContent;
-    makeKopi($kopi, name);
+    $kopi.innerHTML = makeKopi(name);
   }
 
-  function makeKopi($kopi, name, ingredients){
+  function makeKopi(name){
+    // Magic happens from Kopi.js
+    var ingredients = Kopi.parse(name);
 
-      // Magic happens from Kopi.js
-      if (!ingredients) {
-          ingredients = Kopi.parse(name);
-      } else if (!name) {
-          name = Kopi.stringify(ingredients);
+    // Configure the internal ingredients of the Kopi
+    var internalIngredients = 'water condensed_milk evaporated_milk coffee'.split(' ');
+    var internalIngredientsHTML = internalIngredients.map(function(ingredient){
+      var value = ingredients[ingredient];
+      if (!value) return;
+      var height = value * 100 + '%';
+      return '<div class="' + ingredient + '" style="height: ' + height + '"></div>';
+    }).join('');
+
+    // Render the sugar intelligently
+    var sugarHTML = '';
+    if (ingredients.sugar > 0){
+      if (ingredients.sugar <= 0.5){
+        sugarHTML = '<div class="sugar"></div>';
+      } else if (ingredients.sugar <= 1){
+        sugarHTML = '<div class="sugar sugar-double"></div>';
+      } else {
+        sugarHTML = '<div class="sugar sugar-triple"></div>';
       }
-      // Configure the internal ingredients of the Kopi
-      var internalIngredients = 'water condensed_milk evaporated_milk coffee'.split(' ');
-      var internalIngredientsHTML = internalIngredients.map(function(ingredient){
-        var value = ingredients[ingredient];
-        if (!value) return;
-        var height = value * 100 + '%';
-        return '<div class="' + ingredient + '" style="height: ' + height + '"></div>';
-      }).join('');
+    }
 
-      // Render the sugar intelligently
-      var sugarHTML = '';
-      if (ingredients.sugar > 0){
-        if (ingredients.sugar <= 0.5){
-          sugarHTML = '<div class="sugar"></div>';
-        } else if (ingredients.sugar <= 1){
-          sugarHTML = '<div class="sugar sugar-double"></div>';
-        } else {
-          sugarHTML = '<div class="sugar sugar-triple"></div>';
-        }
-      }
+    // Render the (random) ice(s)
+    var randDeg = function(){
+      // Returns -90 to 90 deg
+      return (Math.random()*180).toFixed() - 90;
+    };
+    var randLeft = function(){
+      // Returns 8 to 26
+      return (Math.random()*18 + 8).toFixed();
+    };
+    iceHTML = '<div class="ice-blocks">'
+        + '<div class="ice" style="transform: rotate(' + randDeg() + 'deg); -webkit-transform: rotate(' + randDeg() + 'deg); left: ' + randLeft() + 'px"></div>'
+        + '<div class="ice" style="transform: rotate(' + randDeg() + 'deg); -webkit-transform: rotate(' + randDeg() + 'deg); left: ' + randLeft() + 'px"></div>'
+        + '<div class="ice" style="transform: rotate(' + randDeg() + 'deg); -webkit-transform: rotate(' + randDeg() + 'deg); left: ' + randLeft() + 'px"></div>'
+      + '</div>';
 
-      // Render the (random) ice(s)
-      var randDeg = function(){
-        // Returns -90 to 90 deg
-        return (Math.random()*180).toFixed() - 90;
-      };
-      var randLeft = function(){
-        // Returns 8 to 26
-        return (Math.random()*18 + 8).toFixed();
-      };
-      iceHTML = '<div class="ice-blocks">'
-          + '<div class="ice" style="transform: rotate(' + randDeg() + 'deg); -webkit-transform: rotate(' + randDeg() + 'deg); left: ' + randLeft() + 'px"></div>'
-          + '<div class="ice" style="transform: rotate(' + randDeg() + 'deg); -webkit-transform: rotate(' + randDeg() + 'deg); left: ' + randLeft() + 'px"></div>'
-          + '<div class="ice" style="transform: rotate(' + randDeg() + 'deg); -webkit-transform: rotate(' + randDeg() + 'deg); left: ' + randLeft() + 'px"></div>'
-        + '</div>';
+    var state = ingredients.state;
+    ingredients.state = 'lukewarm';
+    var lukewarmName = Kopi.stringify(ingredients);
+    ingredients.state = 'iced';
+    var icedName = Kopi.stringify(ingredients);
 
-      var state = ingredients.state;
-      ingredients.state = 'lukewarm';
-      var lukewarmName = Kopi.stringify(ingredients);
-      ingredients.state = 'iced';
-      var icedName = Kopi.stringify(ingredients);
-
-      // Finally render the whole Kopi in its full glory
-      var html = '<div class="kopi kopi-' + state + '">'
-          + sugarHTML
-          + '<div class="cup">'
-            + '<div class="ingredients">' + internalIngredientsHTML + '</div>'
-            + iceHTML
-          + '</div>'
-          + '<div class="plate"></div>'
+    // Finally render the whole Kopi in its full glory
+    var html = '<div class="kopi kopi-' + state + '">'
+        + sugarHTML
+        + '<div class="cup">'
+          + '<div class="ingredients">' + internalIngredientsHTML + '</div>'
+          + iceHTML
         + '</div>'
-        + '<div class="kopi-name">'
-          + '<span class="kopi-warm-name">' + name + '</span>'
-          + '<span class="kopi-lukewarm-name">' + lukewarmName + '</span>'
-          + '<span class="kopi-iced-name">' + icedName + '</span>'
-        + '</div>';
-      $kopi.innerHTML = html;
+        + '<div class="plate"></div>'
+      + '</div>'
+      + '<div class="kopi-name">'
+        + '<span class="kopi-warm-name">' + name + '</span>'
+        + '<span class="kopi-lukewarm-name">' + lukewarmName + '</span>'
+        + '<span class="kopi-iced-name">' + icedName + '</span>'
+      + '</div>';
+    return html;
   }
 
-  //Make your own kopi
-  var $recipe = document.getElementById('recipe');
-  var $ownKopi = document.getElementById('your-kopi');
-  $ownKopi.className = 'warm';
-  makeKopi($ownKopi, null, {water: 0.4, coffee: 0.4, condensed_milk: 0.2, state: "warm"});
-  //indication of amount
-  $recipe.addEventListener('input', function(event){
-      var $status = this.querySelector('span#' + event.target.name);
-      $status.innerHTML = event.target.value;
-  })
-  //render kopi
-  $recipe.addEventListener('change', function(event){
-      var $target = event.target;
-      var $water = parseFloat($target.parentElement.water.value);
-      var $coffee = parseFloat($target.parentElement.coffee.value);
-      var $sugar = parseFloat($target.parentElement.sugar.value);
-      var $condensedMilk = parseFloat($target.parentElement.condensed_milk.value);
-      var $evaporatedMilk = parseFloat($target.parentElement.evaporated_milk.value);
-      var $state = $target.parentElement.state.value;
-      if ($water + $coffee + $sugar + $condensedMilk + $evaporatedMilk !== 1) return document.getElementById('your-kopi').innerHTML = '';
-      $ownKopi.className = $state;
-      makeKopi($ownKopi, null, {
-          water: $water,
-          coffee: $coffee,
-          sugar: $sugar,
-          condensed_milk: $condensedMilk,
-          evaporated_milk: $evaporatedMilk,
-          state: $state
-      });
-  })
+  // Kopi maker
+  var $makerItem = document.getElementById('maker-item');
+  var $makerForm = document.getElementById('maker-form');
+  var $makerName = document.getElementById('maker-name');
+  function renderKopi(){
+    var kopiName = ['kopi_milkiness', 'kopi_strength', 'kopi_sweetness', 'kopi_state'].map(function(name){
+      return $makerForm[name].value;
+    }).join(' ').trim();
+    var kopiHTML;
+    try {
+      kopiHTML = makeKopi(kopiName);
+      $makerName.innerHTML = kopiName;
+    } catch (e) {
+      kopiHTML = e.message;
+      $makerName.innerHTML = 'Kopi ???';
+    }
+    $makerItem.innerHTML = kopiHTML;
+  };
+  renderKopi();
+  $makerForm.onsubmit = $makerForm.oninput = $makerForm.onchange = function(e){
+    e.preventDefault();
+    setTimeout(renderKopi, 100);
+    if ($makerItem.scrollIntoViewIfNeeded){
+      $makerItem.scrollIntoViewIfNeeded();
+    } else {
+      $makerItem.scrollIntoView();
+    }
+  }
 
   // Check for our next meetup
   var xhr = new XMLHttpRequest();
